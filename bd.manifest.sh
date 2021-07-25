d583ce555c500d0b3b68eb0c605dba2c81756d69
AXON <axonasif@gmail.com>
2021-07-25 23:20:28 +0600

    feat: Update bigdroid meta format

--- bd.manifest.sh
+++
@@ -1,5 +0,0 @@
-darkmatter https://github.com/bigdroid-hooks/add_remove_files
-darkmatter https://github.com/bigdroid-hooks/custom_ghome
-darkmatter https://github.com/bigdroid-hooks/custom_ramdisk
-darkmatter https://github.com/bigdroid-hooks/default_lang_locale
-darkmatter https://github.com/bigdroid-hooks/overlord_screen
